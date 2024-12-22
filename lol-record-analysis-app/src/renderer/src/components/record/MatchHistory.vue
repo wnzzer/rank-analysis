7a7a4a14d9b12411a5f2448c0981a997346545b9
wnzzer <1503123330@qq.com>
2024-12-22 18:42:00 +0800

    左侧段位数据

--- lol-record-analysis-app/src/renderer/src/components/record/MatchHistory.vue
+++ lol-record-analysis-app/src/renderer/src/components/record/MatchHistory.vue
@@ -1,10 +1,14 @@
 <template>
-<n-flex vertical style="height: 850px;">
-    <div style="background-color: aliceblue;" v-for="i in 10" :key="i" class="item">
+ <n-flex vertical style="height: 85vh;">
+    <div
+      style="background-color: aliceblue; flex: 1; display: flex; justify-content: center; align-items: center;"
+      v-for="i in 10"
+      :key="i"
+      class="item"
+    >
       {{ i }}
     </div>
-</n-flex>
-
+  </n-flex>
 
 </template>
 <script setup lang="ts">
