3b57d057e8d314e076e619dc0b3a22a7528826df
wnzzer <1503123330@qq.com>
2024-12-23 18:19:56 +0800

    渐进色标题

--- lol-record-analysis-app/src/renderer/src/components/record/UserRecord.vue
+++ lol-record-analysis-app/src/renderer/src/components/record/UserRecord.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="user-record-card">
+  <n-flex vertical style="display: flex; position: relative; height: 100vh;">
     <n-card :bordered="false">
       <n-flex>
         <div style="position: relative;">
@@ -30,37 +30,124 @@
     </n-card>
 
     <!-- 这里显示的是RANKED_SOLO_5x5的排名图标 -->
-    <n-card :bordered="true">
-      <n-flex>
-        <div>
-          <img width="70px" height="70px"
-            :src="requireImg(summoner.rank.queueMap.RANKED_SOLO_5x5.tier.toLocaleLowerCase() + '.png')" />
+    <div style="position: relative;">
+
+      <n-card :bordered="true">
+        <div style="position: absolute; left: 0;top: 0;">
+          <span style="transform: translateX(5px);">
+            <n-icon>
+              <heart-dislike></heart-dislike>
+            </n-icon>
+          </span>
+          <span>
+            牛马标签
+          </span>
         </div>
-        <div style="width: 60%;">
-          
+        <n-flex>
+          <div>
+          </div>
+          <div>
+            <n-button style="margin: 5px;" v-for="i in 5" :key="i" size="tiny" type="primary">
+              六连胜
+            </n-button>
+
+          </div>
+        </n-flex>
+      </n-card>
+    </div>
+    <div style="position: relative;">
+
+      <n-card :bordered="false">
+        <div style="position: absolute; left: 0;top: 0;">
+
+          <span>
+            单双排
+          </span>
         </div>
-      </n-flex>
-    </n-card>
-    <n-card :bordered="true">
-      <n-flex>
-        <div>
-          <img width="70px" height="70px"
-            :src="requireImg(summoner.rank.queueMap.RANKED_SOLO_5x5.tier.toLocaleLowerCase() + '.png')" />
+        <n-flex>
+          <div>
+            <img width="70px" height="70px"
+              :src="requireImg(summoner.rank.queueMap.RANKED_SOLO_5x5.tier.toLocaleLowerCase() + '.png')" />
+          </div>
+          <div style="width: 60%;">
+            <n-flex vertical>
+              <RecordButton
+                :record-type="getRecordType(summoner.rank.queueMap.RANKED_SOLO_5x5.wins, summoner.rank.queueMap.RANKED_SOLO_5x5.losses)">
+                胜率：{{ getWinRate(summoner.rank.queueMap.RANKED_SOLO_5x5.wins,
+                  summoner.rank.queueMap.RANKED_SOLO_5x5.losses) }}%
+              </RecordButton>
+              <n-button size="tiny">胜场：{{ summoner.rank.queueMap.RANKED_SOLO_5x5.wins }}</n-button>
+              <n-button size="tiny">负场：{{ summoner.rank.queueMap.RANKED_SOLO_5x5.losses }}</n-button>
+            </n-flex>
+          </div>
+        </n-flex>
+      </n-card>
+    </div>
+    <div style="position: relative;">
+
+      <n-card :bordered="false">
+        <n-flex>
+          <div style="position: absolute; left: 0;top: 0;">
+
+            <span>
+              灵活组排
+            </span>
+          </div>
+          <div>
+            <img width="70px" height="70px"
+              :src="requireImg(summoner.rank.queueMap.RANKED_FLEX_SR.tier.toLocaleLowerCase() + '.png')" />
+          </div>
+          <div style="width: 60%;">
+            <n-flex vertical>
+              <RecordButton
+                :record-type="getRecordType(summoner.rank.queueMap.RANKED_FLEX_SR.wins, summoner.rank.queueMap.RANKED_FLEX_SR.losses)">
+                胜率：{{
+                  getWinRate(summoner.rank.queueMap.RANKED_FLEX_SR.wins, summoner.rank.queueMap.RANKED_FLEX_SR.losses) }}%
+              </RecordButton>
+              <n-button size="tiny">胜场：{{ summoner.rank.queueMap.RANKED_FLEX_SR.wins }}</n-button>
+              <n-button size="tiny">负场：{{ summoner.rank.queueMap.RANKED_FLEX_SR.losses }}</n-button>
+            </n-flex>
+
+          </div>
+        </n-flex>
+      </n-card>
+    </div>
+    <!-- 这里显示的是RANKED_SOLO_5x5的排名图标 -->
+    <div style="position: relative;  flex: 1; flex-grow: 1;">
+
+      <n-card :bordered="true" style=" flex: 1; flex-grow: 1;">
+        <div style="position: absolute; left: 0;top: 0; flex: 1; flex-grow: 1;">
+          <span style="transform: translateX(5px);">
+            <n-icon>
+              <heart-dislike></heart-dislike>
+            </n-icon>
+          </span>
+          <span>
+            数据面板
+          </span>
         </div>
-        <div style="width: 60%;">
-          
-        </div>
-      </n-flex>
-    </n-card>
-  </div>
+        <n-flex>
+          <div>
+          </div>
+          <div>
+
+
+          </div>
+        </n-flex>
+      </n-card>
+    </div>
+
+
+  </n-flex>
 </template>
 
 <script setup lang="ts">
 import http from '@renderer/services/http';
-import { CopyOutline, Server } from '@vicons/ionicons5'
+import { CopyOutline, Server, HeartDislike } from '@vicons/ionicons5'
 import { onMounted, ref } from 'vue';
 
 import { NCard, NFlex, NButton, NIcon, useMessage } from 'naive-ui';
+import RecordButton from './RecordButton.vue';
 
 // 定义 SummonerInfo 接口
 interface Summoner {
@@ -155,7 +242,28 @@ const getSummoner = async (name: string) => {
   console.log(res)
   summoner.value = res.data
 }
+const getWinRate = (win: number, loss: number) => {
+  // 首先检查是否有比赛记录，如果没有则胜率为0
+  if (win + loss === 0) {
+    return 0;
+  }
+  // 计算胜率并转换为百分比形式
+  const winRate = (win / (win + loss)) * 100;
+  // 返回胜率，保留整数部分
+  console.log(winRate)
+  return Math.round(winRate);
+};
+const getRecordType = (win, loss) => {
+  const winRate = getWinRate(win, loss)
+  if (winRate >= 58) {
+    return 'good'
+  } else if (winRate <= 49) {
+    return 'bad'
+  } else {
+    return ''
+  }
 
+}
 /**
 * Returns the image path for the given rank tier.
 * This function dynamically requires the image based on the provided tier string,
