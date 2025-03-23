72b22002ec451273c7dc95fc0f65ed0e3e7b1966
wnzzer <1503123330@qq.com>
2025-03-23 12:53:32 +0800

    添加自动化页,提取英雄选择为公共函数

--- lol-record-analysis-app/src/renderer/src/views/settings/Automation.vue
+++ lol-record-analysis-app/src/renderer/src/views/settings/Automation.vue
@@ -1,45 +1,90 @@
 <template>
-    <n-space vertical>
-      <!-- Display Settings -->
-      <n-card>
-        <n-text tag="div" class="setting-title">基本设置</n-text>
-        
-        <n-space vertical>
-          <div class="setting-item">
-            <span class="setting-label">自动接受对局</span>
-                <n-switch v-model:value="autoAccept" />
+  <n-space vertical>
+    <!-- Display Settings -->
+    <n-card>
+      <n-text tag="div" class="setting-title">基本设置</n-text>
 
-          </div>      
+      <n-space vertical>
+        <div class="setting-item">
+          <span class="setting-label">
+            <n-icon size="20" color="#2080f0"><Flash /></n-icon>
+            自动接受对局
+          </span>
+          <n-switch v-model:value="autoAccept" />
+        </div>
 
+        <div class="setting-item">
+          <span class="setting-label">
+            <n-icon size="20" color="#18a058"><CheckmarkCircle /></n-icon>
+            自动选择英雄
+          </span>
+          <n-switch v-model:value="autoPick" />
+        </div>
+        <n-flex>
+          <VueDraggable ref="el" v-model="myListData">
+            <n-tag v-for="item in myListData" round closeable :bordered="false" style="margin-right: 15px;">
+              {{ item.label }}
+              <template #avatar>
+                <n-avatar src="https://cdnimg103.lizhi.fm/user/2017/02/04/2583325032200238082_160x160.jpg" />
+              </template>
+            </n-tag>
+          </VueDraggable>
+          <n-select v-model:value="selectChampionId" filterable :filter="filterChampionFunc" placeholder="添加英雄"
+            :render-tag="renderSingleSelectTag" :render-label="renderLabel" :options="championOptions" size="small"
+            style="width: 170px" @update:value="handleUpdateValue" />
+        </n-flex>
+        <n-text depth="3" style="font-size: 12px">拖动可以改变选择英雄的优先级</n-text>
+        <div class="setting-item">
+          <span class="setting-label">
+            <n-icon size="20" color="#d03050"><Close /></n-icon>
+            自动禁止英雄
+          </span>
+          <n-switch v-model:value="autoBan" />
+        </div>
+        <n-flex>
+          <VueDraggable ref="el" v-model="myListData">
+          <n-tag v-for="item in myListData" round closeable :bordered="false" style="margin-right: 15px;">
+            {{ item.label }}
+            <template #avatar>
+              <n-avatar src="https://cdnimg103.lizhi.fm/user/2017/02/04/2583325032200238082_160x160.jpg" />
+            </template>
+          </n-tag>
+        </VueDraggable>
+        <n-select v-model:value="selectChampionId" filterable :filter="filterChampionFunc" placeholder="添加英雄"
+          :render-tag="renderSingleSelectTag" :render-label="renderLabel" :options="championOptions" size="small"
+          style="width: 170px" @update:value="handleUpdateValue" />
+        </n-flex>
+        <n-text depth="3" style="font-size: 12px">拖动可以改变禁用英雄的优先级</n-text>
 
-          <div class="setting-item">
-            <span class="setting-label">自动选择英雄</span>
-                <n-switch v-model:value="autoPick" />
+      </n-space>
+    </n-card>
 
-          </div>
-          <div class="setting-item">
-            <span class="setting-label">自动禁止英雄</span>
-                <n-switch v-model:value="autoBan" />
-
-          </div>
-  
-        </n-space>
-      </n-card>
-
-    </n-space>
-  </template>
+  </n-space>
+</template>
 <script setup lang="ts">
+import { VueDraggable } from 'vue-draggable-plus'
 import { ref } from 'vue'
+import { renderSingleSelectTag, renderLabel, championOptions, filterChampionFunc } from '@renderer/components/composition'
+import { CheckmarkCircle, Flash, Close } from '@vicons/ionicons5'
+
 const autoAccept = ref(false)
 const autoPick = ref(false)
 const autoBan = ref(false)
 
+const selectChampionId = ref(null)
 
+const myListData = ref([
+  { label: '山隐之焰', value: 516, realName: '奥恩', nickname: '山羊' },
+  { label: '正义巨像', value: 517, realName: '加里奥', nickname: '雕像' },
+  // More items...
+]);
+
+const handleOrderChange = (newList) => {
+  console.log('Order changed:', newList);
+};
 
 // Helper settings
 </script>
-
-
 
 <style scoped>
 .setting-title {
@@ -57,6 +102,9 @@ const autoBan = ref(false)
 
 .setting-label {
   font-size: 14px;
+  display: flex;
+  align-items: center;
+  gap: 4px;
 }
 
 .radio-label {
