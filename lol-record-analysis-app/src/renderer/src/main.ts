e69be4dc69fe9d226899405de63184bd8b18a988
wnzzer <1503123330@qq.com>
2025-04-07 20:58:38 +0800

    全局滚动条

--- lol-record-analysis-app/src/renderer/src/main.ts
+++ lol-record-analysis-app/src/renderer/src/main.ts
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import naive from 'naive-ui'
+import './global.css'; // 引入全局样式文件
 
 const app = createApp(App)
 app.use(naive)
