346ce666dd9db2048b6496dbc188de69ef5510dd
wnzzer <1503123330@qq.com>
2025-04-07 20:46:43 +0800

    修改为全局

--- lol-record-analysis-app/src/renderer/src/main.ts
+++ lol-record-analysis-app/src/renderer/src/main.ts
@@ -2,7 +2,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import naive from 'naive-ui'
 
 const app = createApp(App)
+app.use(naive)
 app.use(router)
 app.mount('#app')
