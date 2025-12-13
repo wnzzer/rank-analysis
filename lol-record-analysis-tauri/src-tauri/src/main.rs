42453228a38022bb90f8e45115412bc666328b69
wnzzer <1503123330@qq.com>
2025-12-13 15:51:56 +0800

    加依赖

--- lol-record-analysis-tauri/src-tauri/src/main.rs
+++ lol-record-analysis-tauri/src-tauri/src/main.rs
@@ -45,6 +45,7 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_process::init())
         .register_uri_scheme_protocol("asset", move |_app, request| {
             let path = request.uri().path();
             // path is like /champion/123
